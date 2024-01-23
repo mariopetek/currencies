834ec5f871147b1f69e2f69790a3ce8f6edf4234
Mario Petek <mariopetek0@gmail.com>
2024-01-23 17:56:31 +0100

    Header active link fix

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -9,40 +9,32 @@ import { RouterLink } from 'vue-router'
             <h2>Currencies</h2>
         </div>
         <nav>
-            <ul>
-                <li>
-                    <RouterLink to="/" active-class="active"
-                        >Converter</RouterLink
-                    >
-                </li>
-                <li>
-                    <RouterLink to="/rates" active-class="active"
-                        >Rates</RouterLink
-                    >
-                </li>
-            </ul>
+            <RouterLink to="/" active-class="active">Converter</RouterLink>
+            <RouterLink to="/rates" active-class="active">Rates</RouterLink>
         </nav>
     </header>
 </template>
 
 <style scoped>
 header {
+    --header-height: 3rem;
     background-color: var(--color-heading);
     border-bottom: 1px solid var(--color-border);
-    padding: 0.6rem;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 3rem;
+    height: var(--header-height);
     gap: 3rem;
     position: sticky;
     top: 0;
+    z-index: 1;
 }
 .logoContainer {
     display: flex;
     align-items: center;
     gap: 0.5rem;
     height: 100%;
+    padding: 0.6rem;
 }
 .logoContainer > img {
     height: 100%;
@@ -50,17 +42,16 @@ header {
 .logoContainer > h2 {
     font-weight: 600;
 }
-ul {
+nav {
     display: flex;
-    gap: 0.85rem;
-    list-style-type: none;
 }
 a {
     text-decoration: none;
     font-weight: 500;
     color: var(--color-text);
-    padding: 0.85rem 1rem;
+    padding: 0 1rem;
     transition: color 0.2s ease-in-out;
+    line-height: var(--header-height);
 }
 a:hover {
     color: var(--color-brand);
